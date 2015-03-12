bd77dc18c9f67d02df300922ac7e1eb70dffa0f8
Guilherme F. Lima <gflima@sdf.org>
2015-03-12 19:46:59 -0300

    src: Add command-line option support nclua interpreter
    * src/nclua.c: Likewise.
    * TODO: Updated accordingly.

--- src/nclua.c
+++ src/nclua.c
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with NCLua.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <config.h>
-#include <assert.h>
 #include <stdlib.h>
 #include <setjmp.h>
 
@@ -33,20 +32,86 @@ along with NCLua.  If not, see <http://www.gnu.org/licenses/>.  */
 #include "macros.h"
 #include "luax-macros.h"
 
-#define WIDTH  800
-#define HEIGHT 600
+/* Globals: */
 static ncluaw_t *ncluaw_state;
-static GtkWidget *app;
 static jmp_buf panic_jmp;
+
+static GtkWidget *app;
+static gint WIDTH = 800;
+static gint HEIGHT = 600;
+
+/* Options: */
+#define OPTION_LINE "FILE..."
+#define OPTION_DESC                             \
+  "Report bugs to: " PACKAGE_BUGREPORT "\n"     \
+  "NCLua home page: " PACKAGE_URL
+
+static gboolean
+opt_size (arg_unused (const gchar *opt), const gchar *arg,
+          arg_unused (gpointer data), GError **err)
+{
+  guint64 width;
+  guint64 height;
+  gchar *end;
+
+  width = g_ascii_strtoull (arg, &end, 10);
+  if (width == 0)
+    goto syntax_error;
+  WIDTH = (gint) (clamp (width, 0, G_MAXINT));
+
+  if (*end != 'x')
+    goto syntax_error;
+
+  height = g_ascii_strtoull (++end, NULL, 10);
+  if (height == 0)
+    goto syntax_error;
+  HEIGHT = (gint) (clamp (height, 0, G_MAXINT));
+
+  return TRUE;
+
+ syntax_error:
+  g_set_error (err, G_OPTION_ERROR, G_OPTION_ERROR_BAD_VALUE,
+               "Invalid size string '%s'", arg);
+  return FALSE;
+}
+
+static gboolean
+opt_version (void)
+{
+  puts (PACKAGE_STRING);
+  exit (EXIT_SUCCESS);
+}
+
+static GOptionEntry options[] = {
+  {"size", 's', 0, G_OPTION_ARG_CALLBACK,
+   pointerof (opt_size), "Set window size", "WIDTHxHEIGHT"},
+  {"version", 0, G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK,
+   pointerof (opt_version), "Print version information and exit", NULL},
+  {NULL}
+};
+
+/* Error handling: */
+#define usage_error(message) _error (TRUE, message)
+#define print_error(message) _error (FALSE, message)
+
+static void
+_error (gboolean try_help, const gchar *message)
+{
+  const gchar *me = g_get_application_name ();
+  fprintf (stderr, "%s: %s\n", me, message);
+  if (try_help)
+    fprintf (stderr, "Try '%s --help' for more information.\n", me);
+}
 
 static void
 panic (arg_unused (ncluaw_t *nw), const char *message)
 {
   luax_dump_stack (cast (lua_State *, ncluaw_debug_get_lua_state (nw)));
-  fprintf (stderr, "error: %s\n", message);
+  print_error (message);
   longjmp (panic_jmp, 1);
 }
 
+/* Callbacks: */
 #if GTK_CHECK_VERSION(3,8,0)
 static gboolean
 cycle_callback (GtkWidget *canvas, arg_unused (gpointer frame_clock),
@@ -182,25 +247,43 @@ pointer_click_callback (arg_unused (GtkWidget *widget), GdkEventButton *e,
 int
 main (int argc, char **argv)
 {
-  GtkWidget *canvas;
-  char *errmsg = NULL;
-  volatile int status = EXIT_SUCCESS;
+  GOptionContext *ctx;
+  GError *error = NULL;
 
   gchar *dirname;
   gchar *basename;
 
-  if (unlikely (argc != 2))
-    {
-      fprintf (stderr, "usage: nclua [OPTION]... FILE\n");
-      exit (EXIT_FAILURE);
-    }
+  GtkWidget *canvas;
+  char *errmsg = NULL;
+  volatile int status = EXIT_SUCCESS;
 
   gtk_init (&argc, &argv);
 
+  /* Parse command-line options and arguments.  */
+  ctx = g_option_context_new (OPTION_LINE);
+  g_option_context_set_description (ctx, OPTION_DESC);
+  g_option_context_add_main_entries (ctx, options, NULL);
+  g_option_context_add_group (ctx, gtk_get_option_group (TRUE));
+  if (unlikely (!g_option_context_parse (ctx, &argc, &argv, &error)))
+    {
+      usage_error (error->message);
+      g_error_free (error);
+      exit (EXIT_FAILURE);
+    }
+  g_option_context_free (ctx);
+
+  if (unlikely (argc != 2))
+    {
+      usage_error ("Missing file operand");
+      exit (EXIT_FAILURE);
+    }
+
+  /* Setup process working directory. */
   dirname = g_path_get_dirname (argv[1]);
   basename = g_path_get_basename (argv[1]);
-  assert (g_chdir (dirname) == 0);
+  g_assert (g_chdir (dirname) == 0);
 
+  /* Open the NCLua state.  */
   ncluaw_state = ncluaw_open (basename, WIDTH, HEIGHT, &errmsg);
   if (unlikely (ncluaw_state == NULL))
     {
@@ -214,8 +297,9 @@ main (int argc, char **argv)
   g_free (dirname);
   g_free (basename);
 
+  /* Setup GTK+ stuff.  */
   app = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  assert (app != NULL);         /* cannot fail */
+  g_assert (app != NULL);       /* cannot fail */
   gtk_window_set_title (GTK_WINDOW (app), "NCLua");
   gtk_widget_set_size_request (app, WIDTH, HEIGHT);
   gtk_window_set_resizable (GTK_WINDOW (app), FALSE);
@@ -231,7 +315,7 @@ main (int argc, char **argv)
                     deconst (void *, "release"));
 
   canvas = gtk_drawing_area_new ();
-  assert (canvas != NULL);      /* cannot fail */
+  g_assert (canvas != NULL);    /* cannot fail */
   gtk_widget_add_events (canvas, GDK_BUTTON_PRESS_MASK
                          | GDK_BUTTON_RELEASE_MASK
                          | GDK_POINTER_MOTION_MASK);
@@ -256,17 +340,20 @@ main (int argc, char **argv)
   g_timeout_add (1000 / 60, (GSourceFunc) cycle_callback, canvas);
 #endif
 
-  /* NCL */
+  /* Send NCL start event.  */
   ncluaw_send_ncl_event (ncluaw_state, "presentation", "start", "", NULL);
 
+  /* Show interpreter window.  */
   gtk_widget_show_all (app);
 
+  /* Setup panic longjmp stuff.  */
   if (setjmp (panic_jmp))
     {
       status = EXIT_FAILURE;
       goto panic;
     }
 
+  /* Event loop.  */
   gtk_main ();
 
  panic:
